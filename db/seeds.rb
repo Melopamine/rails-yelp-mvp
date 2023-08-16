b432e8f301a5c5a2de93b8da6e858d89d3e218df
anthony lambert <lamberta.mailbox@gmail.com>
2023-08-16 12:36:13 +0200

    finito 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants = [
+  { name: "Restaurant 1", address: "val fleuri", phone_number: "061234", category: "chinese" },
+  { name: "Restaurant 2", address: "route fané", phone_number: "064566", category: "italian" },
+  { name: "Restaurant 3", address: "rue moche", phone_number: "071234", category: "japanese" },
+  { name: "Restaurant 4", address: "dans la mer", phone_number: "072568", category: "french" },
+  { name: "Restaurant 5", address: "sous l'ocean", phone_number: "118-218", category: "belgian" }
+]
+
+restaurants.each do |restaurant_attrs|
+  Restaurant.create!(restaurant_attrs)
+  puts "Created #{restaurant_attrs[:name]}"
+end
+
+puts "Finished!"
